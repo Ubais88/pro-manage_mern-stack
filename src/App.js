7cc1d655e03de7bb8c3a9d336e54d59fe7328585
Ubais88 <moubais2143@gmail.com>
2024-02-19 23:04:58 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import './App.css'
 import Auth from "./pages/auth/Auth.js";
-import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Dashboard from "./components/dashboard/Dashboard.js";
+
+
 
 const App = () => {
+  
   return (
     <div style={{ display: "flex" }}>
       <Routes>
-        <Route path="/" element={<Auth />} />
+      <Route path="/" element={<Auth />} />
+        <Route
+          path="/*"
+          element={
+            <div className="homeComponents">
+              <Sidebar />
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Routes>
+            </div>
+          }
+        />
       </Routes>
     </div>
   );
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Sidebar.module.css";
+import { useNavigate } from "react-router-dom";
+import { LuLayout } from "react-icons/lu";
+import { GoDatabase } from "react-icons/go";
+import { IoSettingsOutline } from "react-icons/io5";
+import { HiOutlineLogout } from "react-icons/hi";
+import CodeSandBox from "../../assets/codesandbox.png";
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const [selectedMenuItem, setSelectedMenuItem] = useState("dashboard");
+
+  const handleMenuItemClick = (e) => {
+    const menuItem = e.target.id;
+    setSelectedMenuItem(menuItem);
+  };
+
+  const logoutClickHandler = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    navigate(`/${selectedMenuItem}`);
+  }, [selectedMenuItem]);
+
+  return (
+    <div>
+      <div className={styles.sidebarContainer}>
+        <div className={styles.routes}>
+          <div className={styles.logoContainer}>
+            <img src={CodeSandBox} alt="logo" className={styles.sandboxLogo} />
+            <h1 className={styles.logoText}>Pro Manage</h1>
+          </div>
+
+          <div className={styles.menuItems}>
+            <div
+              onClick={handleMenuItemClick}
+              className={`${styles.menuItem} ${
+                selectedMenuItem === "dashboard" && styles.selectedMenuItem
+              }`}
+              id="dashboard"
+            >
+              <LuLayout />
+              Board
+            </div>
+            <div
+              className={`${styles.menuItem} ${
+                selectedMenuItem === "analytics" && styles.selectedMenuItem
+              }`}
+              id="analytics"
+              onClick={handleMenuItemClick}
+            >
+              <GoDatabase />
+              Analytics
+            </div>
+            <div
+              className={`${styles.menuItem} ${
+                selectedMenuItem === "settings" && styles.selectedMenuItem
+              }`}
+              id="settings"
+              onClick={handleMenuItemClick}
+            >
+              <IoSettingsOutline />
+              Settings
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.logoutContainer}>
+          <h2 className={styles.logoutText} onClick={logoutClickHandler}>
+            <HiOutlineLogout />
+            Logout
+          </h2>
+        </div>
+      </div>
+      {/* {
+      modalOpen && <Createquiz/>
+    } */}
+    </div>
+  );
+};
+
+export default Sidebar;
